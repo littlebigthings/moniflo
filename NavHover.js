de275f36eff934e00f124ed50b4b4d1d1712658c
Himanshu Raj <himanshurajverma549@gmail.com>
2022-06-29 17:10:36 +0530

    add background color in navigation bar on every page

--- NavHover.js
+++ NavHover.js
@@ -4,19 +4,29 @@ const addCSS = css => document.head.appendChild(document.createElement("style"))
 const checkIsCurrent = () => {
     let currentActiveElm = document.querySelector(".w--current");
     let isBlogOpen = document.querySelector("[data-page='blog']");
-    if (currentActiveElm != undefined) {
+    let navBarElm = document.querySelector(".navbar-wrapper");
+    if (currentActiveElm != undefined && navBarElm != undefined) {
         let getColor = currentActiveElm.getAttribute("data-color");
-        if (getColor != null) {
+        let getBackGroundColor = currentActiveElm.getAttribute("nav-color")
+        if (getColor != null || getBackGroundColor) {
             addCSS(`.w--current{ background:${getColor}; }`)
+            navBarElm.style.backgroundColor = getBackGroundColor;
         }
     }
     else if (isBlogOpen != undefined) {
         let activeLink = document.querySelector("[data-active='blog-link']");
         if (activeLink != undefined) {
             let getColor = activeLink.getAttribute("data-color");
-            if(getColor != null){
-                activeLink.style.background=getColor;
+            let getBackGroundColor = activeLink.getAttribute("nav-color")
+            if (getColor != null) {
+                activeLink.style.background = getColor;
+                navBarElm.style.backgroundColor = getBackGroundColor;
             }
+        }
+    }
+    else {
+        if (document.location.pathname == "/thank-you") {
+            navBarElm.style.backgroundColor = "#F1E68A";
         }
     }
 }
